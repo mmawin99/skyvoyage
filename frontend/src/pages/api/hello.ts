aad71c126e039ba37355dd81052d182bdedcd253
Navin Sae-heng <41543586+mmawin99@users.noreply.github.com>
2025-03-24 18:41:38 +0700

    🧹chore: remove prisma from api

--- frontend/src/pages/api/hello.ts
+++ frontend/src/pages/api/hello.ts
@@ -1,14 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const users = await prisma.user.findMany()
-  res.status(200).json(users)
+  res.status(200).json({ name: "John Doe" });
   // res.status(200).json({ name: "John Doe" });
 }

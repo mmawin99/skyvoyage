74369140b06558c2088604fb3f74ee77f9ac7779
Navin Sae-heng <41543586+mmawin99@users.noreply.github.com>
2025-03-26 16:51:27 +0700

    🧹chore: add output attributes --ndb

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output:"export",
   devIndicators: false
 };
 
